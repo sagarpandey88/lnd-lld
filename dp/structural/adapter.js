a6e7a2103f26475aa0bbb04bc23e9da90c7a4f5b
sagarpandey88 <sagarpandey88@gmail.com>
2024-02-17 21:48:30 +0530

    added adapter pattern sample

--- dp/structural/adapter.js
+++ dp/structural/adapter.js
@@ -1,7 +1,37 @@
 
+
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+function area(rectangle) {
+  return rectangle._width * rectangle._height;
+}
+
+
+class SquareToRectangleAdapter {
+
+  constructor(sq) {
+    this._width = sq.side;
+    this._height = sq.side;
+  }
+
+}
+
+
+
 const run = () => {
-    
-    return "Hello Adapter";
-  };
-  
-  export default run;
+
+
+  // build an adapter called SquareToRectangleAdapter
+  // s.t. we could call
+  //
+  let sq = new Square(10);
+  const areaOfSq = area(new SquareToRectangleAdapter(sq));
+
+  return `Area of Square is ${areaOfSq} using an adapter`;
+};
+
+export default run;
